199a2273ab86c7160998d51e308bc822de1c560f
Fredilein <adrian.zanga@icloud.com>
2018-05-04 19:46:35 +0200

    comments added

--- regression.py
+++ regression.py
@@ -1,33 +1,78 @@
-import numpy as np
+"""
+regression.py
+
+-- Description
+Used for Linear Regression Tasks.
+After constructing an object of this class you can fit a model on some input.
+
+
+-- Functions
+
+
+__init__
+    Constructs a Object and initializes parameters
+
+    @(IN)       ada_lr              -- updates the learning rate in every iteration depending on the gradient. Results in faster convergence
+    @(IN)       learning_rate       -- initial learning rate
+
+
+fit()
+    Takes Data X and labels y. Updates the objects weights and loss
+
+    @IN         X                   -- Data vector. Each row corresponding to a sample of dimension [cols]          
+    @IN         y                   -- Label vector. One dimensional, same amount of rows as X
+    @(IN)       steps               -- Number of iterations. Defaults to 500.
+
+
+predict()
+    Takes Data X and returns a label vector calculated from the weights. fit() needs to be called first.
+
+    @IN         X                   -- Data vector.
+
+"""
+
+
+
+
+# Imports
+
+import numpy             as np
 import matplotlib.pyplot as plt
+
 
 
 
 class LinearRegression:
 
+    C_INC = 1.1
+    C_DEC = 0.5
+
 
     def __init__(self, ada_lr=True, learning_rate=0.0001):
         # Constructor
-        self.weights = []
-        self.scores = []
-        self.loss = []
-        self.ada_lr = ada_lr
+        self.weights       = []
+        self.scores        = []
+        self.loss          = []
+        self.ada_lr        = ada_lr
         self.learning_rate = learning_rate
 
 
     def fit(self, X, y, steps=500):
-        n = len(y)
-        dim = X.shape[1]
-        lr_loss = np.array([])
+
+        n         = len(y)
+        dim       = X.shape[1]
+        lr_loss   = np.array([])
+
         # For adaptive learning_rate
         grad_prev = np.zeros(dim)
-        c_inc = 1.1
-        c_dec = 0.5
 
         # Set arbitrary starting point
-        w = np.zeros(dim)
+        w         = np.zeros(dim)
 
+
+        # Repeat updating weights [steps] times
         for t in range(steps):
+
             # Compute Gradient
             sum = np.zeros(dim)
             for i in range(n):
@@ -39,8 +84,8 @@ class LinearRegression:
 
             # Update learning rate
             if self.ada_lr:
-                if (np.linalg.norm(grad) < np.linalg.norm(grad_prev)):  self.learning_rate *= c_inc
-                else:                                                   self.learning_rate *= c_dec
+                if (np.linalg.norm(grad) < np.linalg.norm(grad_prev)):  self.learning_rate *= C_INC
+                else:                                                   self.learning_rate *= C_DEC
                 grad_prev = grad
 
             # Compute Loss
@@ -49,13 +94,16 @@ class LinearRegression:
                 l1 += (y[i] - np.dot(w.T, X[i])) ** 2
             lr_loss = np.append(lr_loss, l1)
 
+
+        # Write weights and loss to the object
         self.weights = w
-        self.loss = lr_loss
+        self.loss    = lr_loss
 
 
     def predict(self, X):
         if self.weights == []:
             print("Model has no weights yet. Call fit() first.")
             return []
+
         return np.polyval(self.weights, X)
         
